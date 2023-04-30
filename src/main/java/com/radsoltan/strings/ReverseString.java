efa6611bb3bc4eb5c2d3b33c748c97bebcfdfecc
gothaw <rad.soltan@gmail.com>
2023-04-30 18:01:44 +0100

    Added solution for ReverseString

---
+++ src/main/java/com/radsoltan/strings/ReverseString.java
@@ -0,0 +1,8 @@
+package com.radsoltan.strings;
+
+// Community solution the same as mine
+public class ReverseString {
+    public String reverse(String inputString) {
+        return new StringBuilder(inputString).reverse().toString();
+    }
+}
