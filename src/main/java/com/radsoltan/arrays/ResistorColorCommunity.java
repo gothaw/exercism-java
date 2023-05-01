d3b3599108cae4c90ba5bd0a0d18b900ac4bf556
gothaw <rad.soltan@gmail.com>
2023-05-01 13:59:26 +0100

    Refactoring community solution for ResistorColorCommunity

--- src/main/java/com/radsoltan/arrays/ResistorColorCommunity.java
+++ src/main/java/com/radsoltan/arrays/ResistorColorCommunity.java
@@ -3,11 +3,13 @@ package com.radsoltan.arrays;
 import java.util.Arrays;
 
 public class ResistorColorCommunity {
+    private static final String[] COLORS = new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
     int colorCode(String color) {
         return Arrays.asList(colors()).indexOf(color);
     }
 
     String[] colors() {
-        return new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+        return COLORS;
     }
 }
