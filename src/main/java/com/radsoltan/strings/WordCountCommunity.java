64f397bc66b0f4cfa0bf105b962c91464a86d0ca
gothaw <rad.soltan@gmail.com>
2023-07-27 23:56:45 +0100

    Added Word Count community solution using streams

---
+++ src/main/java/com/radsoltan/strings/WordCountCommunity.java
@@ -0,0 +1,21 @@
+package com.radsoltan.strings;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class WordCountCommunity {
+    public Map<String, Integer> phrase(String word) {
+        return Arrays.stream(word
+                        .toLowerCase()
+                        .replaceAll("[^a-z0-9' ]", " ")
+                        .trim()
+                        .split("\\s+"))
+                .map(chunk -> chunk.replaceAll("^'|'$", ""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, chunk -> chunk.getValue().intValue()));
+
+    }
+}
