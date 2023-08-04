47e8130a210cbf6f1747586fa9bc8adc97e2ad71
gothaw <rad.soltan@gmail.com>
2023-08-04 17:06:57 +0100

    Added solution for PigLatinTranslator

--- src/main/java/com/radsoltan/strings/PigLatinTranslator.java
+++ src/main/java/com/radsoltan/strings/PigLatinTranslator.java
@@ -1,9 +1,9 @@
 package com.radsoltan.strings;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PigLatinTranslator {
     private static final Set<Character> VOWELS = new HashSet<>();
@@ -18,34 +18,48 @@ public class PigLatinTranslator {
         SPECIAL_VOWELS.add("yt");
     }
 
-    public String translate(String word) {
-        StringBuilder translated = new StringBuilder();
+    public String translate(String sentence) {
+        return Arrays.stream(sentence.split("\\s")).map(word -> {
+            StringBuilder translated = new StringBuilder();
 
-        if (isBeginWithVowel(word)) {
-            // Rule 1
-            return translated.append(word).append("ay").toString();
-        }
-
-        // Rule 3 and 4
-        StringBuilder cluster = new StringBuilder();
-        int splitIndex = 0;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (!VOWELS.contains(c) && !(c == 'y' && cluster.length() > 1)) {
-                cluster.append(c);
-            } else {
-                splitIndex = i;
-                if (c == 'u' && word.charAt(i - 1) == 'q' && i > 1) {
-                    cluster.append('u');
-                    splitIndex++;
-                }
-                break;
+            if (isRuleOne(word)) {
+                return translated.append(word).append("ay").toString();
             }
-        }
-        return translated.append(word.substring(splitIndex)).append(cluster).append("ay").toString();
+
+            StringBuilder cluster = new StringBuilder();
+            int splitIndex = 0;
+
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (isRuleTwo(c) || isRuleFour(word, cluster, c, i)) {
+                    splitIndex = i;
+                    if (isRuleThree(word, i, c)) {
+                        cluster.append('u');
+                        splitIndex++;
+                    }
+                    break;
+                } else {
+                    cluster.append(c);
+                }
+            }
+
+            return translated.append(word.substring(splitIndex)).append(cluster).append("ay").toString();
+        }).collect(Collectors.joining(" "));
     }
 
-    private boolean isBeginWithVowel(String word) {
+    private boolean isRuleOne(String word) {
         return VOWELS.contains(word.charAt(0)) || SPECIAL_VOWELS.contains(word.substring(0,2));
+    }
+
+    private boolean isRuleTwo(char character) {
+        return VOWELS.contains(character);
+    }
+
+    private boolean isRuleThree(String word, int index, char character) {
+        return character == 'u' && word.charAt(index - 1) == 'q';
+    }
+
+    private boolean isRuleFour(String word, StringBuilder cluster, char character, int index) {
+        return (character == 'y' && cluster.length() > 1) || (word.length() == 2 && index == 1 && character=='y');
     }
 }
